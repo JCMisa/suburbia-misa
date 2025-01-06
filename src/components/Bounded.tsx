c9355290431d8195a05bb1e800005ee6711328eb
MisaJC <johncarlomisa399@gmail.com>
2025-01-06 13:45:13 +0800

    change bounded Comp to section

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -9,13 +9,13 @@ type BoundedProps = {
 };
 
 export function Bounded({
-  as: Comp = "section",
+  // as: Comp = "section",
   className,
   children,
   ...restProps
 }: BoundedProps) {
   return (
-    <Comp
+    <section
       className={clsx(
         "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
         className
@@ -23,6 +23,6 @@ export function Bounded({
       {...restProps}
     >
       <div className="mx-auto w-full max-w-6xl">{children}</div>
-    </Comp>
+    </section>
   );
 }
